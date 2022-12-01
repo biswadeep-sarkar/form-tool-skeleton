72ab50e6344ff2845015ac6050d4f9c28b1c92ae
Biswadeep Sarkar <biswadeepoffice@gmail.com>
2022-12-01 20:59:00 +0530

    Renamed Model

--- app/Models/Admin/DemoModel.php
+++ app/Models/Admin/DemoModel.php
@@ -5,9 +5,9 @@ namespace App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use Biswadeep\FormTool\Models\AdminModel;
+use Biswadeep\FormTool\Models\BaseModel;
 
-class DemoModel extends AdminModel
+class DemoModel extends BaseModel
 {
     use HasFactory;
 
